d75ce243fe7b9f60d45cb1b004beb90c7306a1b5
EugeneGpil <gpilligrim@gmail.com>
2023-07-09 06:27:30 +0300

    1 ;

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/EugeneGpil/go_getting_started
 
 go 1.20
 
-require github.com/EugeneGpil/greetings v0.0.1
+require github.com/EugeneGpil/greetings v0.0.2
--- hello.go
+++ hello.go
@@ -2,12 +2,21 @@ package main
 
 import (
     "fmt"
+    "log"
 
 	"github.com/EugeneGpil/greetings"
 )
 
 func main() {
-    message := greetings.Hello("PES")
+    log.SetPrefix("greetings: ")
+    // log.SetFlags(0)
+
+    // message, err := greetings.Hello("")
+    message, err := greetings.Hello("PES")
+
+    if (err != nil) {
+        log.Fatal(err)
+    }
 
     fmt.Println(message)
 }
